178e5d8daa806fc6aed9a3b7ab5fb1dc2bad2d36
messense <messense@icloud.com>
2021-03-02 12:12:50 +0800

    Minor

--- x86_64-unknown-linux-gnu.rb
+++ x86_64-unknown-linux-gnu.rb
@@ -4,12 +4,12 @@ class X8664UnknownLinuxGnu < Formula
   license "GPL-3.0-or-later" => { with: "GCC-exception-3.1" }
 
   if Hardware::CPU.arm?
-    url = "https://github.com/messense/macos-cross-toolchains/releases/download/v1.1.0/x86_64-unknown-linux-gnu-aarch64-darwin-glibc-2.12.1.zip"
-    version = "10.2.0"
-    sha256 = "b0e6f6734890dcb69ec00b3476936a4d8a296625e41fb344f3a955bcb0e2f9bf"
+    url "https://github.com/messense/macos-cross-toolchains/releases/download/v1.1.0/x86_64-unknown-linux-gnu-aarch64-darwin-glibc-2.12.1.zip"
+    version "10.2.0"
+    sha256 "b0e6f6734890dcb69ec00b3476936a4d8a296625e41fb344f3a955bcb0e2f9bf"
   else
-    url = "https://github.com/messense/macos-cross-toolchains/releases/download/v1.1.0/x86_64-unknown-linux-gnu-x86_64-darwin-glibc-2.12.1.zip"
-    sha256 = "be2989980202a23a51ee0a8f2e0b44c980169f25c980dce9a02d0826f0111ee4"
+    url "https://github.com/messense/macos-cross-toolchains/releases/download/v1.1.0/x86_64-unknown-linux-gnu-x86_64-darwin-glibc-2.12.1.zip"
+    sha256 "be2989980202a23a51ee0a8f2e0b44c980169f25c980dce9a02d0826f0111ee4"
   end
 
   def install
