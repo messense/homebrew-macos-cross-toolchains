7e148f8a49da7e23e1d43ed25d6abc47882f89f8
messense <messense@icloud.com>
2021-03-03 12:04:16 +0800

    Add armv7-unknown-linux-gnueabihf x86_64 toolchain

--- armv7-unknown-linux-gnueabihf.rb
+++ armv7-unknown-linux-gnueabihf.rb
@@ -8,9 +8,9 @@ class Armv7UnknownLinuxGnueabihf < Formula
     version "10.2.0"
     sha256 "754cce3e8e455fe1d32bb8de69354b8118ee12b7dd0c8da7e033471010c154ea"
   else
-    url ""
+    url "https://github.com/messense/homebrew-macos-cross-toolchains/releases/download/v1.1.0/armv7-unknown-linux-gnueabihf-x86_64-darwin.zip"
     version "10.2.0"
-    sha256 ""
+    sha256 "0d2ebfd8d3964b345d0740b6caeb7ce419cea4a2706e64ecbeb50539ec65ff75"
   end
 
   def install
