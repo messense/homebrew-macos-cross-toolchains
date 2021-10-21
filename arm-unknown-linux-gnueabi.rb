6749b7527489473a569c8f09ef87aa6517deeeb4
messense <messense@icloud.com>
2021-10-21 17:43:22 +0800

    Add Arm v6 formula for x86_64

---
+++ arm-unknown-linux-gnueabi.rb
@@ -0,0 +1,19 @@
+class ArmUnknownLinuxGnueabi < Formula
+  desc "arm Linux GNU eabi Toolchain"
+  homepage "https://github.com/messense/homebrew-macos-cross-toolchains"
+  license "GPL-3.0-or-later" => { with: "GCC-exception-3.1" }
+  version "10.3.0"
+
+  if Hardware::CPU.arm?
+    url "https://github.com/messense/homebrew-macos-cross-toolchains/releases/download/v10.3.0/arm-unknown-linux-gnueabi-aarch64-darwin.tar.gz"
+    sha256 ""
+  else
+    url "https://github.com/messense/homebrew-macos-cross-toolchains/releases/download/v10.3.0/arm-unknown-linux-gnueabi-x86_64-darwin.tar.gz"
+    sha256 "f9b2c1726e61bf1cb33376f5996a8bbb8a254ffd0dcb98c500b5994dc143c2a3"
+  end
+
+  def install
+    (prefix/"toolchain").install Dir["./*"]
+    Dir.glob(prefix/"toolchain/bin/*") {|file| bin.install_symlink file}
+  end
+end
---
+++ arm-unknown-linux-gnueabihf.rb
@@ -0,0 +1,19 @@
+class ArmUnknownLinuxGnueabihf < Formula
+  desc "arm Linux GNU eabihf Toolchain"
+  homepage "https://github.com/messense/homebrew-macos-cross-toolchains"
+  license "GPL-3.0-or-later" => { with: "GCC-exception-3.1" }
+  version "10.3.0"
+
+  if Hardware::CPU.arm?
+    url "https://github.com/messense/homebrew-macos-cross-toolchains/releases/download/v10.3.0/arm-unknown-linux-gnueabihf-aarch64-darwin.tar.gz"
+    sha256 ""
+  else
+    url "https://github.com/messense/homebrew-macos-cross-toolchains/releases/download/v10.3.0/arm-unknown-linux-gnueabihf-x86_64-darwin.tar.gz"
+    sha256 "d5153917d06ad5a26ce798e967909c8ee8106fcd9c791890401e5e48f7e9848f"
+  end
+
+  def install
+    (prefix/"toolchain").install Dir["./*"]
+    Dir.glob(prefix/"toolchain/bin/*") {|file| bin.install_symlink file}
+  end
+end
