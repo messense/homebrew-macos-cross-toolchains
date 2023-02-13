9c391566c80897d4b16698644a50aa0e145c1fff
Paul Spooren <paul.spooren@rhebo.com>
2023-02-13 16:49:21 +0100

    arm-unknown-linux-musleabihf: add target
    Signed-off-by: Paul Spooren <paul.spooren@rhebo.com>

---
+++ arm-unknown-linux-musleabihf.rb
@@ -0,0 +1,24 @@
+class ArmUnknownLinuxMusleabihf < Formula
+  desc "arm Linux MUSL eabihf Toolchain"
+  homepage "https://github.com/messense/homebrew-macos-cross-toolchains"
+  license "GPL-3.0-or-later" => { with: "GCC-exception-3.1" }
+  version "11.2.0"
+
+  depends_on "bdw-gc"
+  depends_on "guile"
+  depends_on "zstd"
+  depends_on "python@3.10"
+
+  if Hardware::CPU.arm?
+    url "https://github.com/messense/homebrew-macos-cross-toolchains/releases/download/v11.2.0/arm-unknown-linux-musleabihf-aarch64-darwin.tar.gz"
+    sha256 "0089dae44e7cde8b3b7e133ea6401887134144a0e159b8b6a2a8be4816910cbf"
+  else
+    url "https://github.com/messense/homebrew-macos-cross-toolchains/releases/download/v11.2.0/arm-unknown-linux-musleabihf-x86_64-darwin.tar.gz"
+    sha256 "68c6a53b4dbf6cba229efebe1d2768e087089291ac8cfb74b8cebca8ddd6bdfa"
+  end
+
+  def install
+    (prefix/"toolchain").install Dir["./*"]
+    Dir.glob(prefix/"toolchain/bin/*") {|file| bin.install_symlink file}
+  end
+end
