27d0005cac058acdea0f983cf7bfc33f7a7dd55d
messense <messense@icloud.com>
2023-05-28 13:03:04 +0800

    Add mipsel-unkown-linux-gnu formula

---
+++ mipsel-unknown-linux-gnu.rb
@@ -0,0 +1,25 @@
+class MipselUnknownLinuxGnu < Formula
+  desc "mipsel Linux GNU Toolchain"
+  homepage "https://github.com/messense/homebrew-macos-cross-toolchains"
+  license "GPL-3.0-or-later" => { with: "GCC-exception-3.1" }
+  version "11.2.0"
+  revision 1
+
+  depends_on "bdw-gc"
+  depends_on "guile"
+  depends_on "zstd"
+  depends_on "python@3.10"
+
+  if Hardware::CPU.arm?
+    url "https://github.com/messense/homebrew-macos-cross-toolchains/releases/download/v11.2.0-1/mipsel-unknown-linux-gnu-aarch64-darwin.tar.gz"
+    sha256 "9878c769d5944e94f734f0ea72655a9c18e814898435766dea6426e0df02438e"
+  else
+    url "https://github.com/messense/homebrew-macos-cross-toolchains/releases/download/v11.2.0-1/mipsel-unknown-linux-gnu-x86_64-darwin.tar.gz"
+    sha256 "f5729b9a07d94cc505dd8d025343af985f98ca851030ee9396ceba7be282cab6"
+  end
+
+  def install
+    (prefix/"toolchain").install Dir["./*"]
+    Dir.glob(prefix/"toolchain/bin/*") {|file| bin.install_symlink file}
+  end
+end
